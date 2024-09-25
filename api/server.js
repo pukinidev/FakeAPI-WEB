34914743cfa56bd52c5da691450be8a71c73fce4
pukinidev <v.caro@uniandes.edu.co>
2024-09-25 15:27:17 -0500

    changes

--- api/server.js
+++ api/server.js
@@ -3,17 +3,26 @@ const jsonServer = require('json-server')
 
 const server = jsonServer.create()
 
-const router = jsonServer.router('db.json')
+const fs = require('fs')
+const path = require('path')
+const filePath = path.join('db.json')
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db)
+
 
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
+
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
+    
 }))
 server.use(router)
 server.listen(3000, () => {
     console.log('JSON Server is running')
 })
 
+// Export the Server API
 module.exports = server
